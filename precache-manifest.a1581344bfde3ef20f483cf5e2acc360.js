9fd8d9fce1a35b02486ca460791fcdc44f9dbfa1
jangapalli vyaskumar <vyaskumar.vyaskumar@gmail.com>
2022-05-31 15:42:53 +0530

    Updates

---
+++ precache-manifest.a1581344bfde3ef20f483cf5e2acc360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8b65116b480ac86c65d",
+    "url": "/to-do-list/static/css/main.e8011c1f.chunk.css"
+  },
+  {
+    "revision": "f8b65116b480ac86c65d",
+    "url": "/to-do-list/static/js/main.f8b65116.chunk.js"
+  },
+  {
+    "revision": "61016c177af3f8558ef6",
+    "url": "/to-do-list/static/js/1.61016c17.chunk.js"
+  },
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "ec64aff6c2291e617634a3f8f3ba75ed",
+    "url": "/to-do-list/index.html"
+  }
+];
